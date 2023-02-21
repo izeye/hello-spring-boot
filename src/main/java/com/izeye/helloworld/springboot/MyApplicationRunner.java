5c74ff212fa54c43f2038b9154d0ee0da50f7792
Johnny Lim <izeye@naver.com>
2023-02-22 03:21:03 +0900

    Apply Spring native support

--- src/main/java/com/izeye/helloworld/springboot/MyApplicationRunner.java
+++ src/main/java/com/izeye/helloworld/springboot/MyApplicationRunner.java
@@ -16,9 +16,19 @@ public class MyApplicationRunner implements ApplicationRunner {
 
     private static final Logger log = LoggerFactory.getLogger(MyApplicationRunner.class);
 
+    private final PersonService personService;
+
+    public MyApplicationRunner(PersonService personService) {
+        this.personService = personService;
+    }
+
     @Override
     public void run(ApplicationArguments args) {
         log.info("Hello, Spring Boot!");
+
+        Person person = this.personService.getPerson(1L);
+        log.info("Hello, {}!", person.getFirstName());
+        log.info("Cache is working: {}", this.personService.getPerson(1L) == person);
     }
 
 }
