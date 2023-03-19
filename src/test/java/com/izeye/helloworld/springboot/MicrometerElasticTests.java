5c987f5f72e6db605eac7fb1bc95e5945e8030b3
Johnny Lim <izeye@naver.com>
2023-03-20 02:19:04 +0900

    Add tests for https://github.com/micrometer-metrics/micrometer/issues/1882

---
+++ src/test/java/com/izeye/helloworld/springboot/MicrometerElasticTests.java
@@ -0,0 +1,34 @@
+package com.izeye.helloworld.springboot;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for Micrometer.
+ *
+ * @author Johnny Lim
+ */
+@SpringBootTest
+@AutoConfigureObservability
+class MicrometerElasticTests {
+
+    @Autowired
+    private MeterRegistry meterRegistry;
+
+    @Test
+    void counter() {
+        Counter counter = this.meterRegistry.counter("my.counter");
+        assertThat(counter.count()).isEqualTo(0);
+        counter.increment();
+        assertThat(counter.count()).isEqualTo(0);
+        this.meterRegistry.close();
+        assertThat(counter.count()).isEqualTo(1);
+    }
+
+}
