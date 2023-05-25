7c52a31ad155b1703892582540de2aba1eca67cb
Johnny Lim <izeye@naver.com>
2023-05-25 14:16:55 +0900

    Enable percentiles for HTTP server/client request metrics

--- src/main/java/com/izeye/helloworld/springboot/TestController.java
+++ src/main/java/com/izeye/helloworld/springboot/TestController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * {@link RestController} for testing.
@@ -14,9 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/test")
 public class TestController {
 
+    private final WebClient webClient;
+
+    public TestController(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
     @GetMapping("/httpServletRequest")
     public String http(HttpServletRequest request) {
         return "name='%s'".formatted(request.getParameter("name"));
+    }
+
+    @GetMapping("/webClient")
+    public String webClient() {
+        return this.webClient.get().uri("https://www.google.com/").retrieve().toEntity(String.class).block().getBody();
     }
 
 }
