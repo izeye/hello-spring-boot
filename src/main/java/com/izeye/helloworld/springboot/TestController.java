133424b1a7859c3f25c64dc77943c12cb3b2207d
Johnny Lim <izeye@naver.com>
2023-10-05 08:54:04 +0900

    Add an endpoint for testing latency

--- src/main/java/com/izeye/helloworld/springboot/TestController.java
+++ src/main/java/com/izeye/helloworld/springboot/TestController.java
@@ -1,9 +1,14 @@
 package com.izeye.helloworld.springboot;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * {@link RestController} for testing.
@@ -14,9 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/test")
 public class TestController {
 
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping("/httpServletRequest")
     public String http(HttpServletRequest request) {
         return "name='%s'".formatted(request.getParameter("name"));
+    }
+
+    @GetMapping("/sleep")
+    public String sleep(@RequestParam int seconds) throws InterruptedException {
+        log.info("I'll sleep for {} seconds.", seconds);
+        TimeUnit.SECONDS.sleep(seconds);
+        log.info("I slept for {} seconds.", seconds);
+        return "I slept for %d seconds.".formatted(seconds);
     }
 
 }
