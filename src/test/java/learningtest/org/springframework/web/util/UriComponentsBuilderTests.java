35c185bbb99655c062b619acfab68216ad6312a1
Johnny Lim <izeye@naver.com>
2024-03-29 08:45:46 +0900

    Add tests for UriComponentsBuilder

---
+++ src/test/java/learningtest/org/springframework/web/util/UriComponentsBuilderTests.java
@@ -0,0 +1,23 @@
+package learningtest.org.springframework.web.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link UriComponentsBuilder}.
+ *
+ * @author Johnny Lim
+ */
+class UriComponentsBuilderTests {
+
+    @Test
+    void test() {
+        URI uri = UriComponentsBuilder.fromHttpUrl("https://httpbin.org/headers").queryParam("a", "test").queryParam("b", "테스트").build().encode().toUri();
+        assertThat(uri).hasToString("https://httpbin.org/headers?a=test&b=%ED%85%8C%EC%8A%A4%ED%8A%B8");
+    }
+
+}
