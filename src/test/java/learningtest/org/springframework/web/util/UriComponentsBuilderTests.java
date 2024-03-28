36632d28c45c037282c3898360a2465827801440
Johnny Lim <izeye@naver.com>
2024-03-29 08:46:18 +0900

    Polish

--- src/test/java/learningtest/org/springframework/web/util/UriComponentsBuilderTests.java
+++ src/test/java/learningtest/org/springframework/web/util/UriComponentsBuilderTests.java
@@ -16,7 +16,12 @@ class UriComponentsBuilderTests {
 
     @Test
     void test() {
-        URI uri = UriComponentsBuilder.fromHttpUrl("https://httpbin.org/headers").queryParam("a", "test").queryParam("b", "테스트").build().encode().toUri();
+        URI uri = UriComponentsBuilder.fromHttpUrl("https://httpbin.org/headers")
+                .queryParam("a", "test")
+                .queryParam("b", "테스트")
+                .build()
+                .encode()
+                .toUri();
         assertThat(uri).hasToString("https://httpbin.org/headers?a=test&b=%ED%85%8C%EC%8A%A4%ED%8A%B8");
     }
 
