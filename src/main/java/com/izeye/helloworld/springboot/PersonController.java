9308a82677619307160f386a9fe3cca4299adaea
Johnny Lim <izeye@naver.com>
2024-04-08 16:47:39 +0900

    Add sample for ContentCachingRequestWrapper

--- src/main/java/com/izeye/helloworld/springboot/PersonController.java
+++ src/main/java/com/izeye/helloworld/springboot/PersonController.java
@@ -1,11 +1,18 @@
 package com.izeye.helloworld.springboot;
 
+import jakarta.servlet.ServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 /**
@@ -17,13 +24,17 @@ import java.util.List;
 @RequestMapping(path = "/persons")
 public class PersonController {
 
+    private static final Logger log = LoggerFactory.getLogger(PersonController.class);
+
     @GetMapping
     public List<Person> getPersons() {
         return List.of(new Person("Johnny", "Lim"));
     }
 
     @PostMapping
-    public Person addPerson(@RequestBody Person person) {
+    public Person addPerson(@RequestBody Person person, ServletRequest request) throws IOException {
+        log.info("request.getInputStream(): {}", StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8));
+        log.info("((ContentCachingRequestWrapper) request).getContentAsString(): {}", ((ContentCachingRequestWrapper) request).getContentAsString());
         return person;
     }
 
---
+++ src/main/java/com/izeye/helloworld/springboot/TestFilter.java
@@ -0,0 +1,27 @@
+package com.izeye.helloworld.springboot;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+import java.io.IOException;
+
+/**
+ * {@link Filter} for testing.
+ *
+ * @author Johnny Lim
+ */
+@Component
+public class TestFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(new ContentCachingRequestWrapper(request), response);
+    }
+
+}
