6877f5373c3bc898c676e67f28420698855e8f95
Johnny Lim <izeye@naver.com>
2024-11-21 17:54:12 +0900

    Add tests for UriComponentsBuilder.getQueryParams()

--- src/test/java/learningtest/org/springframework/web/util/UriComponentsBuilderTests.java
+++ src/test/java/learningtest/org/springframework/web/util/UriComponentsBuilderTests.java
@@ -1,6 +1,7 @@
 package learningtest.org.springframework.web.util;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -25,6 +26,14 @@ class UriComponentsBuilderTests {
                 .encode()
                 .toUri();
         assertThat(uri).hasToString("https://httpbin.org/headers?a=test&b=%ED%85%8C%EC%8A%A4%ED%8A%B8&empty=&null");
+    }
+
+    @Test
+    void getQueryParams() {
+        String url = "https://httpbin.org/headers?a=test&b=%ED%85%8C%EC%8A%A4%ED%8A%B8";
+        MultiValueMap<String, String> queryParams = UriComponentsBuilder.fromHttpUrl(url).build().getQueryParams();
+        assertThat(queryParams.get("a")).containsExactly("test");
+        assertThat(queryParams.get("b")).containsExactly("%ED%85%8C%EC%8A%A4%ED%8A%B8");
     }
 
 }
