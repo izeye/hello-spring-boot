086f404bea73669fe406073473c49319b74d57ae
Johnny Lim <izeye@naver.com>
2025-01-24 13:41:50 +0900

    Apply Logback access

--- src/main/java/com/izeye/helloworld/springboot/WebConfig.java
+++ src/main/java/com/izeye/helloworld/springboot/WebConfig.java
@@ -1,5 +1,10 @@
 package com.izeye.helloworld.springboot;
 
+import ch.qos.logback.access.servlet.TeeFilter;
+import ch.qos.logback.access.tomcat.LogbackValve;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
@@ -15,6 +20,23 @@ public class WebConfig {
     @Bean
     public MappingJackson2JsonView jsonView() {
         return new MappingJackson2JsonView();
+    }
+
+    @Bean
+    public FilterRegistrationBean<TeeFilter> teeFilter() {
+        FilterRegistrationBean<TeeFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new TeeFilter());
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
+        return (factory) -> {
+            LogbackValve logbackValve = new LogbackValve();
+            logbackValve.setFilename("logback-access.xml");
+
+            factory.addContextValves(logbackValve);
+        };
     }
 
 }
