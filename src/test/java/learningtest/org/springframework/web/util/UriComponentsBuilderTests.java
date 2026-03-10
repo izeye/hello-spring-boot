b77db8938b6b8e8dd1c078d6c424239db9d52af9
Johnny Lim <izeye@naver.com>
2026-03-10 22:10:52 +0900

    Add tests for UriComponents.encode() and UriComponentsBuilder.encode()

--- src/test/java/learningtest/org/springframework/web/util/UriComponentsBuilderTests.java
+++ src/test/java/learningtest/org/springframework/web/util/UriComponentsBuilderTests.java
@@ -7,6 +7,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 /**
  * Tests for {@link UriComponentsBuilder}.
@@ -17,7 +18,7 @@ class UriComponentsBuilderTests {
 
     @Test
     void test() {
-        URI uri = UriComponentsBuilder.fromHttpUrl("https://httpbin.org/headers")
+        URI uri = UriComponentsBuilder.fromUriString("https://httpbin.org/headers")
                 .queryParam("a", "test")
                 .queryParam("b", "테스트")
                 .queryParam("empty", "")
@@ -29,9 +30,29 @@ class UriComponentsBuilderTests {
     }
 
     @Test
+    void buildAndEncodeWithUriTemplateLikeValue() {
+        URI uri = UriComponentsBuilder.fromUriString("https://httpbin.org/headers")
+                .queryParam("a", "a{b}c")
+                .build()
+                .encode() // UriComponents.encode()
+                .toUri();
+        assertThat(uri).hasToString("https://httpbin.org/headers?a=a%7Bb%7Dc");
+    }
+
+    @Test
+    void encodeAndBuildWithUriTemplateLikeValue() {
+        assertThatIllegalStateException()
+                .isThrownBy(() -> UriComponentsBuilder.fromUriString("https://httpbin.org/headers")
+                        .queryParam("a", "a{b}c")
+                        .encode() // UriComponentsBuilder.encode()
+                        .build()
+                        .toUri());
+    }
+
+    @Test
     void getQueryParams() {
         String url = "https://httpbin.org/headers?a=test&b=%ED%85%8C%EC%8A%A4%ED%8A%B8";
-        MultiValueMap<String, String> queryParams = UriComponentsBuilder.fromHttpUrl(url).build().getQueryParams();
+        MultiValueMap<String, String> queryParams = UriComponentsBuilder.fromUriString(url).build().getQueryParams();
         assertThat(queryParams.get("a")).containsExactly("test");
         assertThat(queryParams.get("b")).containsExactly("%ED%85%8C%EC%8A%A4%ED%8A%B8");
     }
